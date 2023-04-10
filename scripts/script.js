ea8d6162f919053dc1bf850bf44aaef41023a643
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2023-04-10 15:31:12 +0300

    сделала вторую страницу

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,34 @@
+const team_content = document.querySelector('.about-company_team_content');
+const vacancies_content = document.querySelector('.about-company_vacancies_content');
+const about_titles = document.querySelectorAll('.about-company_text');
+const lisencies_content = document.querySelector('.about-company_lisencies_content');
 function searchAppear(event) {
     event.target.nextElementSibling.style.display = 'block';
 }
 
+function chooseTeam(event) {
+    event.target.classList.add('active');
+    about_titles[1].classList.remove('active');
+    about_titles[2].classList.remove('active');
+    team_content.classList.add('block');
+    vacancies_content.classList.remove('block');
+    lisencies_content.classList.remove('block');
+}
+
+function chooseVacancies(event) {
+    event.target.classList.add('active');
+    about_titles[0].classList.remove('active');
+    about_titles[2].classList.remove('active');
+    team_content.classList.remove('block');
+    vacancies_content.classList.add('block');
+    lisencies_content.classList.remove('block');
+}
+
+function chooseLisences(event) {
+    event.target.classList.add('active');
+    about_titles[0].classList.remove('active');
+    about_titles[1].classList.remove('active');
+    team_content.classList.remove('block');
+    vacancies_content.classList.remove('block');
+    lisencies_content.classList.add('block');
+}
